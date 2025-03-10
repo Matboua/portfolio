d1261ee9c7f26a3c9ecf3f1af58ff84bafdedce3
matboua <matbouaoussama@gmail.com>
2025-03-10 23:06:01 +0000

    add language selection and mode toggle components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import { Logs } from "lucide-react";
-import ChooseLanguage from "./ChooseLanguage";
-import Mode from "./Mode";
+import ChooseLanguage from "./features/ChooseLanguage";
+import Mode from "./features/Mode";
 import { useState } from "react";
+import Menu from "./features/Menu";
 export default function Header() {
 	const [isNav, setIsNav] = useState(false);
 	return (
 		<header
-			className="fixed top-0 flex justify-center w-full sm:pb-4 pt-4 bg-white/55 dark:bg-gray-900/80 backdrop-blur-sm  before:content-[''] before:absolute before:top-0 before:cursor-pointer  before:-left-[100%] before:w-full before:h-full before:bg-gradient-to-r dark:before:from-gray-700/10 dark:before:via-gray-700/20 dark:before:to-gray-700/10  before:from-gray-300/10 before:via-gray-300/20 before:to-gray-300/10 hover:before:-left-[-100%] before:transition-all before:duration-500 after:content[''] after:absolute after:bottom-0 after:left-[-13%] after:w-[126%] after:rounded-full after:h-[2.5px] after:bg-gradient-to-r after:from-pink-900 after:to-blue-900 after:transition-all after:duration-700
-		"
+			className={`fixed top-0 flex justify-center w-full pt-4 bg-white/55 dark:bg-gray-900/80 backdrop-blur-sm  before:content-[''] before:absolute before:top-0 before:cursor-pointer  before:-left-[100%] before:w-full before:h-full before:bg-gradient-to-r dark:before:from-gray-700/10 dark:before:via-gray-700/20 dark:before:to-gray-700/10  before:from-gray-300/10 before:via-gray-300/20 before:to-gray-300/10 hover:before:-left-[-100%] before:transition-all before:duration-500 after:content[''] after:absolute after:bottom-0 after:left-[-13%] after:w-[126%] after:rounded-full after:h-[2.5px] after:bg-gradient-to-r after:from-pink-900 after:to-blue-900 after:transition-all after:duration-700 ${
+				isNav ? "pb-3" : "pb-4.5"
+			}`}
 		>
 			<section className="flex items-center flex-col w-full">
 				<div className="flex w-full px-5 md:px-0 md:w-11/12 lg:w-10/12 justify-between items-center">
@@ -64,54 +66,7 @@ export default function Header() {
 						</div>
 					</div>
 				</div>
-				{isNav && (
-					<div className="flex justify-center top-14 left-0 w-full">
-						<div className=" w-10/12 px-6 flex flex-col gap-5 pt-8 pb-2 font-medium">
-							<a
-								href="#home"
-								className="md:hidden relative hover:bg-gradient-to-r hover:from-pink-900 hover:to-blue-900 hover:text-transparent hover:bg-clip-text before:content[''] before:absolute before:-bottom-1 before:-left-2 before:w-0 hover:before:w-16 before:rounded-full before:h-[2.5px] hover:before:bg-gradient-to-r hover:before:from-pink-600 hover:before:to-blue-900 before:transition-all before:duration-700 hover:after:content[''] hover:after:absolute hover:after:-top-2.5 hover:after:-left-26 after:w-0 hover:after:w-[200%] hover:after:h-[200%] dark:hover:after:bg-gray-500/10 hover:after:bg-gray-700/10 after:transition-all after:duration-500"
-							>
-								Home
-							</a>
-							<a
-								href="#about"
-								className="md:hidden relative hover:bg-gradient-to-r hover:from-pink-900 hover:to-blue-900 hover:text-transparent hover:bg-clip-text before:content[''] before:absolute before:-bottom-1 before:-left-2 before:w-0 hover:before:w-16 before:rounded-full before:h-[2.5px] hover:before:bg-gradient-to-r hover:before:from-pink-600 hover:before:to-blue-900 before:transition-all before:duration-700 hover:after:content[''] hover:after:absolute hover:after:-top-2.5 hover:after:-left-26 after:w-0 hover:after:w-[200%] hover:after:h-[200%] dark:hover:after:bg-gray-500/10 hover:after:bg-gray-700/10 after:transition-all after:duration-500"
-							>
-								About
-							</a>
-							<a
-								href="#experience"
-								className="md:hidden relative hover:bg-gradient-to-r hover:from-pink-900 hover:to-blue-900 hover:text-transparent hover:bg-clip-text before:content[''] before:absolute before:-bottom-1 before:-left-2 before:w-0 hover:before:w-23 before:rounded-full before:h-[2.5px] hover:before:bg-gradient-to-r hover:before:from-pink-600 hover:before:to-blue-900 before:transition-all before:duration-700 hover:after:content[''] hover:after:absolute hover:after:-top-2.5 hover:after:-left-26 after:w-0 hover:after:w-[200%] hover:after:h-[200%] dark:hover:after:bg-gray-500/10 hover:after:bg-gray-700/10 after:transition-all after:duration-500"
-							>
-								Experience
-							</a>
-							<a
-								href="#projects"
-								className="md:hidden relative hover:bg-gradient-to-r hover:from-pink-900 hover:to-blue-900 hover:text-transparent hover:bg-clip-text before:content[''] before:absolute before:-bottom-1 before:-left-2 before:w-0 hover:before:w-19 before:rounded-full before:h-[2.5px] hover:before:bg-gradient-to-r hover:before:from-pink-600 hover:before:to-blue-900 before:transition-all before:duration-700 hover:after:content[''] hover:after:absolute hover:after:-top-2.5 hover:after:-left-26 after:w-0 hover:after:w-[200%] hover:after:h-[200%] dark:hover:after:bg-gray-500/10 hover:after:bg-gray-700/10 after:transition-all after:duration-500"
-							>
-								Projects
-							</a>
-							<a
-								href="#skills"
-								className="relative hover:bg-gradient-to-r hover:from-pink-900 hover:to-blue-900 hover:text-transparent hover:bg-clip-text before:content[''] before:absolute before:-bottom-1 before:-left-2 before:w-0 hover:before:w-14  before:rounded-full before:h-[2.5px] hover:before:bg-gradient-to-r hover:before:from-pink-600 hover:before:to-blue-900 before:transition-all before:duration-700 hover:after:content[''] hover:after:absolute hover:after:-top-2.5 hover:after:-left-56 after:w-0 hover:after:w-[200%] hover:after:h-[200%] dark:hover:after:bg-gray-500/10 hover:after:bg-gray-700/10 after:transition-all after:duration-500"
-							>
-								Skills
-							</a>
-							<a
-								href="#education"
-								className="relative hover:bg-gradient-to-r hover:from-pink-900 hover:to-blue-900 hover:text-transparent hover:bg-clip-text before:content[''] before:absolute before:-bottom-1 before:-left-2 before:w-0 hover:before:w-22 before:rounded-full before:h-[2.5px] hover:before:bg-gradient-to-r hover:before:from-pink-600 hover:before:to-blue-900 before:transition-all before:duration-700 hover:after:content[''] hover:after:absolute hover:after:-top-2.5 hover:after:-left-56 after:w-0 hover:after:w-[200%] hover:after:h-[200%] dark:hover:after:bg-gray-500/10 hover:after:bg-gray-700/10 after:transition-all after:duration-500"
-							>
-								Education
-							</a>
-							<a
-								href="#contact"
-								className="relative hover:bg-gradient-to-r hover:from-pink-900 hover:to-blue-900 hover:text-transparent hover:bg-clip-text before:content[''] before:absolute before:-bottom-1 before:-left-2 before:w-0 hover:before:w-19 before:rounded-full before:h-[2.5px] hover:before:bg-gradient-to-r hover:before:from-pink-600 hover:before:to-blue-900 before:transition-all before:duration-700 hover:after:content[''] hover:after:absolute hover:after:-top-2.5 hover:after:-left-56 after:w-0 hover:after:w-[200%] hover:after:h-[231%] dark:hover:after:bg-gray-500/10 hover:after:bg-gray-700/10 after:transition-all after:duration-500"
-							>
-								Contact
-							</a>
-						</div>
-					</div>
-				)}
+				{isNav && <Menu />}
 			</section>
 		</header>
 	);
