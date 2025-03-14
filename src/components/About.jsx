c43d3125d5efb7e248ddeb69324cf0a4df10c3c0
matboua <matbouaoussama@gmail.com>
2025-03-14 00:28:06 +0000

    update About component for improved layout and text styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ import { useTranslation } from "react-i18next";
 export default function About() {
 	const { t } = useTranslation();
 	return (
-		<section className="h-screen flex flex-col justify-center items-center">
-			<h1 className="text-5xl">{t("coming.title")}</h1>
-			<p className="opacity-80 mt-1">{t("coming.description")}</p>
+		<section className="h-screen flex flex-col justify-center items-center px-5">
+			<h1 className="text-2xl sm:text-5xl">🙏 {t("coming.title")}</h1>
+			<p className="opacity-80 mt-1 text-center">{t("coming.description")}</p>
 		</section>
 	);
 }
