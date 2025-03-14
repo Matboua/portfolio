5790c3243bcee5e1f07d6543c162b2260ca70d45
matboua <matbouaoussama@gmail.com>
2025-03-14 00:23:40 +0000

    update resume PDF to reflect new education and experience details, improve layout and formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 export default function App() {
@@ -5,6 +6,7 @@ export default function App() {
 		<div className="w-full dark:text-neutral-300 text-neutral-800 dark:bg-neutral-900 bg-neutral-100 pt-[76px] overflow-x-hidden  flex flex-col items-center">
 			<Header />
 			<HeroSection />
+			<About />
 		</div>
 	);
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 export default function About() {
+	const { t } = useTranslation();
 	return (
-		<section>
-			<h1>About</h1>
+		<section className="h-screen flex flex-col justify-center items-center">
+			<h1 className="text-5xl">{t("coming.title")}</h1>
+			<p className="opacity-80 mt-1">{t("coming.description")}</p>
 		</section>
 	);
 }
