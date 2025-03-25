65a2bf18009a70c872077e424c3a07d979fc6747
matboua <matbouaoussama@gmail.com>
2025-03-25 06:26:03 +0000

    add Projects component to showcase project categories and details

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import Projects from "./components/Projects";
 export default function App() {
 	return (
 		<div className="w-full dark:text-neutral-300 text-neutral-800 dark:bg-neutral-900 bg-neutral-100 pt-[76px] overflow-x-hidden  flex flex-col items-center">
 			<Header />
 			<HeroSection />
+			<Projects />
 			<About />
 		</div>
 	);
