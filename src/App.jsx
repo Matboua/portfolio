d5e4358120c6c3abe3de94bd58d2761d3fe0d0d0
matboua <matbouaoussama@gmail.com>
2025-03-27 10:51:02 +0000

    update App component text color for better visibility in dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HeroSection from "./components/HeroSection";
 import Projects from "./components/projects/Projects";
 export default function App() {
 	return (
-		<div className="w-full dark:text-neutral-300 text-neutral-800 dark:bg-neutral-900 bg-neutral-100 pt-[76px] overflow-x-hidden  flex flex-col items-center">
+		<div className="w-full dark:text-neutral-100 text-neutral-800 dark:bg-neutral-900 bg-neutral-100 pt-[76px] overflow-x-hidden  flex flex-col items-center">
 			<Header />
 			<HeroSection />
 			<Projects />
